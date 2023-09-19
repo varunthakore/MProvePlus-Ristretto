eebb68e4cd892c60b172f4d8b44289f795ba6b54
Varun Thakore <vrnthakore@gmail.com>
2023-09-20 02:27:57 +0530

    Update logs

--- gen_log_omnires.sh
+++ gen_log_omnires.sh
@@ -1,16 +1,10 @@
 cargo build --release
-cargo run --release --bin omnires_v2_ristretto_bin 21000 3000 >> out_omni.txt
-echo "Gen for 3000 done !"
+cargo run --release --bin omnires_v2_ristretto_bin 45000 1000 >> out_omni.txt
+echo "Gen for 1000 done !"
 echo $'\n' >> out_omni.txt
-cargo run --release --bin omnires_v2_ristretto_bin 21000 7000 >> out_omni.txt
-echo "Gen for 7000 done !"
+cargo run --release --bin omnires_v2_ristretto_bin 45000 500 >> out_omni.txt
+echo "Gen for 500 done !"
 echo $'\n' >> out_omni.txt
-cargo run --release --bin omnires_v2_ristretto_bin 21000 10000 >> out_omni.txt
-echo "Gen for 10000 done !"
-echo $'\n' >> out_omni.txt
-cargo run --release --bin omnires_v2_ristretto_bin 21000 15000 >> out_omni.txt
-echo "Gen for 15000 done !"
-echo $'\n' >> out_omni.txt
-cargo run --release --bin omnires_v2_ristretto_bin 21000 20000 >> out_omni.txt
-echo "Gen for 20000 done !"
+cargo run --release --bin omnires_v2_ristretto_bin 45000 250 >> out_omni.txt
+echo "Gen for 250 done !"
 echo $'\n' >> out_omni.txt
