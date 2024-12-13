f8d6f12e3e89eac9adb333e9daf382dda98d4cf2
Varun Thakore <vrnthakore@gmail.com>
2024-12-13 13:54:56 +0530

    Update log script

--- gen_log_omnires.sh
+++ gen_log_omnires.sh
@@ -1,140 +1,17 @@
 cargo build --release
-cargo run --release --bin omnires_v2_ristretto_bin 15000 1000 >> out_omni_15.txt
+cargo run --release --bin omnires_v2_ristretto_bin 10000 500 >> ./logs/out_omni_10.txt
+echo "Gen for 500 done !"
+cargo run --release --bin omnires_v2_ristretto_bin 15000 1000 >> ./logs/out_omni_15.txt
 echo "Gen for 1000 done !"
-echo $'\n' >> out_omni_15.txt
-cargo run --release --bin omnires_v2_ristretto_bin 15000 3000 >> out_omni_15.txt
+cargo run --release --bin omnires_v2_ristretto_bin 20000 3000 >> ./logs/out_omni_20.txt
 echo "Gen for 3000 done !"
-echo $'\n' >> out_omni_15.txt
-cargo run --release --bin omnires_v2_ristretto_bin 15000 5000 >> out_omni_15.txt
+cargo run --release --bin omnires_v2_ristretto_bin 25000 5000 >> ./logs/out_omni_25.txt
 echo "Gen for 5000 done !"
-echo $'\n' >> out_omni_15.txt
-cargo run --release --bin omnires_v2_ristretto_bin 15000 7000 >> out_omni_15.txt
+cargo run --release --bin omnires_v2_ristretto_bin 30000 7000 >> ./logs/out_omni_30.txt
 echo "Gen for 7000 done !"
-echo $'\n' >> out_omni_15.txt
-cargo run --release --bin omnires_v2_ristretto_bin 15000 10000 >> out_omni_15.txt
+cargo run --release --bin omnires_v2_ristretto_bin 35000 10000 >> ./logs/out_omni_35.txt
 echo "Gen for 10000 done !"
-echo $'\n' >> out_omni_15.txt
-
-cargo run --release --bin omnires_v2_ristretto_bin 20000 1000 >> out_omni_20.txt
-echo "Gen for 1000 done !"
-echo $'\n' >> out_omni_20.txt
-cargo run --release --bin omnires_v2_ristretto_bin 20000 3000 >> out_omni_20.txt
-echo "Gen for 3000 done !"
-echo $'\n' >> out_omni_20.txt
-cargo run --release --bin omnires_v2_ristretto_bin 20000 5000 >> out_omni_20.txt
-echo "Gen for 5000 done !"
-echo $'\n' >> out_omni_20.txt
-cargo run --release --bin omnires_v2_ristretto_bin 20000 7000 >> out_omni_20.txt
-echo "Gen for 7000 done !"
-echo $'\n' >> out_omni_20.txt
-cargo run --release --bin omnires_v2_ristretto_bin 20000 10000 >> out_omni_20.txt
-echo "Gen for 10000 done !"
-echo $'\n' >> out_omni_20.txt
-cargo run --release --bin omnires_v2_ristretto_bin 20000 15000 >> out_omni_20.txt
+cargo run --release --bin omnires_v2_ristretto_bin 40000 15000 >> ./logs/out_omni_40.txt
 echo "Gen for 15000 done !"
-echo $'\n' >> out_omni_20.txt
-
-cargo run --release --bin omnires_v2_ristretto_bin 25000 1000 >> out_omni_25.txt
-echo "Gen for 1000 done !"
-echo $'\n' >> out_omni_25.txt
-cargo run --release --bin omnires_v2_ristretto_bin 25000 3000 >> out_omni_25.txt
-echo "Gen for 3000 done !"
-echo $'\n' >> out_omni_25.txt
-cargo run --release --bin omnires_v2_ristretto_bin 25000 5000 >> out_omni_25.txt
-echo "Gen for 5000 done !"
-echo $'\n' >> out_omni_25.txt
-cargo run --release --bin omnires_v2_ristretto_bin 25000 7000 >> out_omni_25.txt
-echo "Gen for 7000 done !"
-echo $'\n' >> out_omni_25.txt
-cargo run --release --bin omnires_v2_ristretto_bin 25000 10000 >> out_omni_25.txt
-echo "Gen for 10000 done !"
-echo $'\n' >> out_omni_25.txt
-cargo run --release --bin omnires_v2_ristretto_bin 25000 15000 >> out_omni_25.txt
-echo "Gen for 15000 done !"
-echo $'\n' >> out_omni_25.txt
-cargo run --release --bin omnires_v2_ristretto_bin 25000 20000 >> out_omni_25.txt
+cargo run --release --bin omnires_v2_ristretto_bin 45000 20000 >> ./logs/out_omni_45.txt
 echo "Gen for 20000 done !"
-echo $'\n' >> out_omni_25.txt
-
-cargo run --release --bin omnires_v2_ristretto_bin 30000 1000 >> out_omni_30.txt
-echo "Gen for 1000 done !"
-echo $'\n' >> out_omni_30.txt
-cargo run --release --bin omnires_v2_ristretto_bin 30000 3000 >> out_omni_30.txt
-echo "Gen for 3000 done !"
-echo $'\n' >> out_omni_30.txt
-cargo run --release --bin omnires_v2_ristretto_bin 30000 5000 >> out_omni_30.txt
-echo "Gen for 5000 done !"
-echo $'\n' >> out_omni_30.txt
-cargo run --release --bin omnires_v2_ristretto_bin 30000 7000 >> out_omni_30.txt
-echo "Gen for 7000 done !"
-echo $'\n' >> out_omni_30.txt
-cargo run --release --bin omnires_v2_ristretto_bin 30000 10000 >> out_omni_30.txt
-echo "Gen for 10000 done !"
-echo $'\n' >> out_omni_30.txt
-cargo run --release --bin omnires_v2_ristretto_bin 30000 15000 >> out_omni_30.txt
-echo "Gen for 15000 done !"
-echo $'\n' >> out_omni_30.txt
-
-cargo run --release --bin omnires_v2_ristretto_bin 35000 1000 >> out_omni_35.txt
-echo "Gen for 1000 done !"
-echo $'\n' >> out_omni_35.txt
-cargo run --release --bin omnires_v2_ristretto_bin 35000 3000 >> out_omni_35.txt
-echo "Gen for 3000 done !"
-echo $'\n' >> out_omni_35.txt
-cargo run --release --bin omnires_v2_ristretto_bin 35000 5000 >> out_omni_35.txt
-echo "Gen for 5000 done !"
-echo $'\n' >> out_omni_35.txt
-cargo run --release --bin omnires_v2_ristretto_bin 35000 7000 >> out_omni_35.txt
-echo "Gen for 7000 done !"
-echo $'\n' >> out_omni_35.txt
-cargo run --release --bin omnires_v2_ristretto_bin 35000 10000 >> out_omni_35.txt
-echo "Gen for 10000 done !"
-echo $'\n' >> out_omni_35.txt
-cargo run --release --bin omnires_v2_ristretto_bin 35000 15000 >> out_omni_35.txt
-echo "Gen for 15000 done !"
-echo $'\n' >> out_omni_35.txt
-cargo run --release --bin omnires_v2_ristretto_bin 35000 20000 >> out_omni_35.txt
-echo "Gen for 20000 done !"
-echo $'\n' >> out_omni_35.txt
-
-cargo run --release --bin omnires_v2_ristretto_bin 40000 1000 >> out_omni_40.txt
-echo "Gen for 1000 done !"
-echo $'\n' >> out_omni_40.txt
-cargo run --release --bin omnires_v2_ristretto_bin 40000 3000 >> out_omni_40.txt
-echo "Gen for 3000 done !"
-echo $'\n' >> out_omni_40.txt
-cargo run --release --bin omnires_v2_ristretto_bin 40000 5000 >> out_omni_40.txt
-echo "Gen for 5000 done !"
-echo $'\n' >> out_omni_40.txt
-cargo run --release --bin omnires_v2_ristretto_bin 40000 7000 >> out_omni_40.txt
-echo "Gen for 7000 done !"
-echo $'\n' >> out_omni_40.txt
-cargo run --release --bin omnires_v2_ristretto_bin 40000 10000 >> out_omni_40.txt
-echo "Gen for 10000 done !"
-echo $'\n' >> out_omni_40.txt
-cargo run --release --bin omnires_v2_ristretto_bin 40000 15000 >> out_omni_40.txt
-echo "Gen for 15000 done !"
-echo $'\n' >> out_omni_40.txt
-
-cargo run --release --bin omnires_v2_ristretto_bin 45000 1000 >> out_omni_45.txt
-echo "Gen for 1000 done !"
-echo $'\n' >> out_omni_45.txt
-cargo run --release --bin omnires_v2_ristretto_bin 45000 3000 >> out_omni_45.txt
-echo "Gen for 3000 done !"
-echo $'\n' >> out_omni_45.txt
-cargo run --release --bin omnires_v2_ristretto_bin 45000 5000 >> out_omni_45.txt
-echo "Gen for 5000 done !"
-echo $'\n' >> out_omni_45.txt
-cargo run --release --bin omnires_v2_ristretto_bin 45000 7000 >> out_omni_45.txt
-echo "Gen for 7000 done !"
-echo $'\n' >> out_omni_45.txt
-cargo run --release --bin omnires_v2_ristretto_bin 45000 10000 >> out_omni_45.txt
-echo "Gen for 10000 done !"
-echo $'\n' >> out_omni_45.txt
-cargo run --release --bin omnires_v2_ristretto_bin 45000 15000 >> out_omni_45.txt
-echo "Gen for 15000 done !"
-echo $'\n' >> out_omni_45.txt
-cargo run --release --bin omnires_v2_ristretto_bin 45000 20000 >> out_omni_45.txt
-echo "Gen for 20000 done !"
-echo $'\n' >> out_omni_45.txt
-
